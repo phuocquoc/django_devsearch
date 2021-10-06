ad765738c5c3af108839b159a94b3a4723a365f4
DESKTOP-OJHKNFO\Phuoc Quoc <phuocquocnguyen@gmail.com>
2021-10-06 22:41:10 +0700

    UI

--- users/forms.py
+++ users/forms.py
@@ -7,3 +7,8 @@ class CustomerUser(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerUser, self).__init__(*args, **kwargs)
+        for name, fields in self.fields.items():
+            fields.widget.attrs.update({'class': 'input'})
